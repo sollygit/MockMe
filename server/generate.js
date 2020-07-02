990c4b0949b732a8fc333eb88f3df6a79d98a0fd
Solly <mail4solly@gmail.com>
2020-07-02 21:25:42 +1000

    mock countries

--- server/generate.js
+++ server/generate.js
@@ -1,7 +1,8 @@
 var faker = require('faker');
 
-var database = { product: [] };
+var database = { product: [], country:[] };
 
+// Fake products
 for (var i = 1; i <= 10; i++) {
   const productName = faker.commerce.productName();
   database.product.push({
@@ -11,6 +12,16 @@ for (var i = 1; i <= 10; i++) {
     price: faker.commerce.price(),
     imageUrl: `https://source.unsplash.com/1600x900/?${productName}`,
     quantity: faker.random.number()
+  });
+}
+
+// Fake countries
+for (var i = 1; i <= 5; i++) {
+  const address = faker.address;
+  database.country.push({
+    countryId: i,
+    countryName: address.country(),
+    countryCode: address.countryCode()
   });
 }
 
